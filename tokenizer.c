a8516616331527ea8577bdc467a9c08126f0ee74
tsaist1 <shengtsetsai@gmail.com>
2023-11-11 00:32:19 -0800

    add unary +, -

--- tokenizer.c
+++ tokenizer.c
@@ -193,16 +193,25 @@ Node *expr()
 
 Node *mul()
 {
-    Node *node = primary();
+    Node *node = unary();
     for (;;)
     {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
+}
+
+Node *unary()
+{
+    if (consume('+'))
+        return primary();
+    if (consume('-'))
+        return new_node(ND_SUB, new_node_num(0), primary());
+    return primary();
 }
 
 Node *primary()
@@ -215,15 +224,6 @@ Node *primary()
         return node;
     }
     return new_node_num(expect_number());
-}
-
-Node *unary()
-{
-    if (consume('+'))
-        return primary();
-    if (consume('-'))
-        return new_node(ND_SUB, new_node_num(0), primary());
-    return primary();
 }
 
 // ** Code generator *********************************************************
